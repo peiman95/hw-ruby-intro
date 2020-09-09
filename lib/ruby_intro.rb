91e423b9835018e8bf181c1c308b06b1f08f6aa6
EC2 Default User <ec2-user@ip-172-31-13-146.us-east-2.compute.internal>
2020-09-09 00:32:01 +0000

    ruby_intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,70 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  arr.sort.last(2).sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  arr.combination(2).map.any? { |x, y| x + y == n }
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  s =~ /\A(?=[^aeiou])(?=[a-z])/i
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s == "" || s.delete('01') != ''
+    return false
+  end
+  return s.to_i % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.empty? || price <= 0
+      raise ArgumentError
+    end
+    @isbn = isbn  
+    @price = price
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def isbn=(isbn)
+    @isbn=isbn
+  end
+  
+  def price=(price)
+    @price=price
+  end
+  
+  def price_as_string
+    str = "$%.2f" % [@price]
+    return str
+  end
+  
 end
